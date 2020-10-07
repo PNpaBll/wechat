09752e96db76a48a78670dd6b56ba352e3df479a
PNpaBll <changli19968@126.com>
2020-10-07 17:44:28 +0800

    Yeah, first API usage Done :octocat:

--- pages/rose/rose.js
+++ pages/rose/rose.js
@@ -3,8 +3,25 @@
 
 Page({
 	data:{
-		miao: null
+		miao: null,
+		inputValue: ''
 	},
+
+	onShareAppMessage: function () {
+	    return {
+	      title: '自定义分享标题',
+	      path: '/page/user?id=123',
+	      success: function(res) {
+	        // 分享成功
+	   		console.log('分享成功', res)
+	      },
+	      fail: function(res) {
+	        // 分享失败
+	        console.log('分享失败', res)
+	      }
+	    }
+	},
+
 	goIndex: function(){
 		wx.navigateTo({
 		  url: '../index/index'
@@ -16,12 +33,67 @@ Page({
 		})
 	},
 
+	requestApi: function(){
+		wx.request({
+			url: 'https://api.github.com/',
+			data: {},
+			method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+			// header: {}, // 设置请求的 header
+			success: function(res){
+				console.log('success', res)
+				// success
+			},
+			fail: function(res) {
+				console.log('fail', res)
+				// fail
+			},
+			complete: function(res) {
+				console.log('complete', res)
+				// complete
+			}
+		})
+	},
+
 	popMsg: function(){
 		wx.showToast({
 		  title: '成功',
 		  icon: 'success',
 		  duration: 2000,
 		  mask: true
+		})
+	},
+
+	popScan: function(){
+		wx.scanCode({
+			success: function(res){
+				console.log(res)
+			},
+			fail: function(res) {
+				// fail
+			},
+			complete: function(res) {
+				// complete
+			}
+		})
+	},
+
+	popSetting: function(){
+		wx.openSetting({
+		  success: (res) => {
+		  	console.log(res)
+		    /*
+		     * res.authSetting = {
+		     *   "scope.userInfo": true,
+		     *   "scope.userLocation": true
+		     * }
+		     */
+		  }
+		})
+	},
+
+	getInputValue: function( event ){
+		this.setData({
+			inputValue: event.detail.value
 		})
 	}
 })
